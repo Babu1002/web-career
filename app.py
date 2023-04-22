9bccad351ec7449e11fd56029f2e05fca1019a1d
Babu1002 <111623545+Babu1002@users.noreply.github.com>
2023-04-22 07:44:28 +0000

    Added dynamic data and api rougte

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
+JOBS = [{
+  'id': 1,
+  'title': 'Data analyst',
+  'location': 'delhi, India',
+  'salary': '6,00,000'
+}, {
+  'id': 2,
+  'title': 'Data sciencetist',
+  'location': 'kolkata, India',
+  'salary': '1,00,000'
+}, {
+  'id': 2,
+  'title': 'Back-end devloper',
+  'location': 'Remote',
+  'salary': '50,000'
+}, {
+  'id': 3,
+  'title': 'Front-end devloper',
+  'location': 'Remote'
+}]
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
-if __name__=="__main__":
+
+@app.route("/api/jobs")
+def list_job():
+  return jsonify(JOBS)
+
+
+if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
